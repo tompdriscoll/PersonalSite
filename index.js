b8cdf5be5afe5671242f6992388a8961d78cec43
Tom Driscoll <tdriscoll1@yahoo.com>
2020-07-01 11:57:58 -0400

    fixed animation transform that unfixed fixed elements

--- index.js
+++ index.js
@@ -5,10 +5,14 @@ window.addEventListener('DOMContentLoaded', () => {
     let name = document.getElementById('name')
     let x = document.getElementById('descrip-x')
     let columnHold = document.getElementById('column-hold')
+    let col2 = document.getElementById('column2')
+    let col3 = document.getElementById('column3')
     name.addEventListener('click', ()=> {
-        columnHold.style.transform = 'translate3d(-50vw, 0px, 0px)'
+        col2.style.transform = 'translate3d(-50vw, 0px, 0px)'
+        col3.style.transform = 'translate3d(-50vw, 0px, 0px)'
     })
     x.addEventListener('click', ()=> {
-        columnHold.style.transform = 'translate3d(0vw, 0px, 0px)'
+        col2.style.transform = 'translate3d(0vw, 0px, 0px)'
+        col3.style.transform = 'translate3d(0vw, 0px, 0px)'
     })
 })
