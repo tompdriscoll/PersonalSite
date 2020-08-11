6967e288b05e25c6adb7ca09197a00c11a1bcde4
Tom Driscoll <tdriscoll1@yahoo.com>
2020-08-11 16:44:34 -0400

    fixed lists

--- skills.js
+++ skills.js
@@ -1,10 +1,24 @@
-let skills = document.getElementById('skills-list').children
+let skills = Array.from(document.getElementById('skills-list').children)
+let contact = Array.from(document.getElementById('contact-list').children)
 
 let delay = .7
-Array.from(skills).forEach(skill => {
+// Array.from(skills).forEach(skill => {
 
-skill.style.transitionDelay = `${delay}s`
-skill.style.transform = 'matrix(1,0,0,1,0,0)'
-skill.style.opacity = 1
-delay += .1
-})
+// skill.style.transitionDelay = `${delay}s`
+// skill.style.transform = 'matrix(1,0,0,1,0,0)'
+// skill.style.opacity = 1
+// delay += .1
+// })
+
+for(let i=0; i<=skills.length; i++){
+    
+    skills[i].style.transitionDelay = `${delay}s`
+    skills[i].style.transform = 'matrix(1,0,0,1,0,0)'
+    skills[i].style.opacity = 1
+    
+    if(contact[i]){
+    contact[i].style.transitionDelay = `${delay}s`
+    contact[i].style.transform = 'matrix(1,0,0,1,0,0)'
+    contact[i].style.opacity = 1}
+    delay += .1
+}
